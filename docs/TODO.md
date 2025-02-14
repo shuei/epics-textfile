303ee198a63e5ab7e9b18f625c0df5d9e932290b
Shuei Yamada <shuei@post.kek.jp>
2025-02-14 15:46:44 +0900

    Preliminary implemetation of longout record support.

--- docs/TODO.md
+++ docs/TODO.md
@@ -1,18 +1,20 @@
 TODO list for textfile support
 ====
 
-- [x] Add ai record support
-  - [x] Support NaN / Inf
-  - [ ] Support IEEE754 binary64 format and binary32 format
-- [x] Add longin record support
-  - [x] Support hexadecimal values
-- [ ] Add bi record support
-- [x] Add stringin record support
-- [ ] Add lsi record support
-- [x] Add waveform record support
-  - [x] Support NaN / Inf
-  - [x] Support hexadecimal values
-  - [ ] Support IEEE754 binary64 format and binary32 format
+- [x] Add ai record support.
+  - [x] Support NaN / Inf.
+  - [ ] Support IEEE754 binary64 format and binary32 format.
+- [x] Add longin record support.
+  - [x] Support hexadecimal values.
+- [ ] Add bi record support.
+- [x] Add stringin record support.
+- [ ] Add lsi record support.
+- [x] Add waveform record support.
+  - [x] Support NaN / Inf.
+  - [x] Support hexadecimal values.
+  - [ ] Support IEEE754 binary64 format and binary32 format.
 - [x] Add option to keep input file opened rather than re-open on every process.
   - [ ] Check if inode number has been changed associated to the filename.
 - [x] Check if FTVL is valid within init_record().
+- [x] Output record support which writes to file.
+  - [x] longout record support.
---
+++ src/devTextFileLo.c
@@ -0,0 +1,202 @@
+// -*- coding: utf-8; mode: c; c-basic-offiset: 4 -*-
+
+//////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2020 High Energy Accelerator Research Organization (KEK)
+//
+// text file Device Support 0.0.0
+// and higher are distributed subject to a Software License Agreement found
+// in file LICENSE that is included with this distribution.
+//
+// Author: Shuei Yamada (shuei@post.kek.jp)
+//
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/utsname.h>
+#include <unistd.h>
+
+//
+#include "longoutRecord.h"
+#include "cantProceed.h"
+#include "dbAccess.h"
+#include "devSup.h"
+#include "alarm.h"
+#include "errlog.h"
+#include "recGbl.h"
+#include "link.h"
+#include "epicsExport.h"
+#include "epicsTime.h"
+
+//
+#include "devTextFile.h"
+
+//
+static int devTextFileLoDebug = 0;
+
+/***************************************************************
+ * longout (command/response IO)
+ ***************************************************************/
+static long init(void);
+static long init_record(struct longoutRecord *);
+static long write_lo(struct longoutRecord *);
+
+struct {
+    long        number;
+    DEVSUPFUN   report;
+    DEVSUPFUN   init;
+    DEVSUPFUN   init_record;
+    DEVSUPFUN   get_ioint_info;
+    DEVSUPFUN   write_lo;
+    DEVSUPFUN   special_linconv;
+} devTextFileLo = {
+    6,
+    NULL,
+    init,
+    init_record,
+    NULL,
+    write_lo,
+    NULL
+};
+
+epicsExportAddress(dset, devTextFileLo);
+
+//
+static long init(void)
+{
+    return 0;
+}
+
+//
+static long init_record(struct longoutRecord *prec)
+{
+    DBLINK *plink = &prec->out;
+
+    //
+    if (devTextFileLoDebug>0) {
+        printf("%s (devTextFileLo) filename: %s\n", prec->name, plink->value.instio.string);
+    }
+
+    // Link type must be INST_IO
+    if (plink->type != INST_IO) {
+        errlogPrintf("%s (devTextFileLo): address type must be \"INST_IO\"\n", prec->name);
+        prec->pact = 1;
+        return -1;
+    }
+
+    // Allocate private data storage area
+    TextFile_t *dpvt = callocMustSucceed(1, sizeof(TextFile_t), "calloc for private_t failed ");
+    prec->dpvt = dpvt;
+    dpvt->fp = NULL;
+
+    // Extract output filename
+    char *pstr = plink->value.instio.string;
+
+    if (pstr[0] == '+') {
+        // keep file opened and rewind on process
+        errlogPrintf("%s (devTextFileLo): Keep file opened is not supported\n", prec->name);
+        prec->pact = 1;
+        return -1;
+    }
+
+    const size_t fsize = strlen(pstr) + 1;
+    //if (fsize > MAX_INSTIO_STRING) {
+    //    errlogPrintf("%s (devTextFileLo): INP field is too long\n", prec->name);
+    //    return -1;
+    //}
+    dpvt->name = callocMustSucceed(1, fsize, "calloc for filename failed");
+    strcpy(dpvt->name, pstr);
+
+    // Save inode number
+    struct stat sb;
+    if (stat(dpvt->name, &sb) == -1) {
+        errlogPrintf("%s (devTextFileLo): %s : %s\n", prec->name, dpvt->name, strerror(errno));
+        prec->pact = 1;
+        return -1;
+    }
+    dpvt->ino = sb.st_ino;
+
+    return 0;
+}
+
+//
+static long write_lo(struct longoutRecord *prec)
+{
+    //DBLINK *plink = &prec->inp;
+    TextFile_t *dpvt = prec->dpvt;
+    const char *filename = dpvt->name;
+
+    //
+    if (devTextFileLoDebug>0) {
+        printf("%s (devTextFileLo): filename: %s\n", prec->name, filename);
+    }
+
+    //
+    FILE *fp = fopen(filename, "w");
+    if (fp == NULL) {
+        errlogPrintf("%s (devTextFileLo): can't open \"%s\"\n", prec->name, filename);
+        prec->nsev = INVALID_ALARM;
+        prec->nsta = WRITE_ACCESS_ALARM;
+        return -1;
+    }
+
+    //
+    int retval = 0;
+
+    // timestamp
+    char datetime[128];
+    epicsTimeToStrftime(datetime, sizeof(datetime), "%Y-%m-%d %T", &prec->time);
+
+    char wday[32];
+    epicsTimeToStrftime(wday, sizeof(wday), "(%a)", &prec->time);
+
+    // hostname
+    // gethostname() won't work if /etc/hostname is empty
+    struct utsname buf;
+    uname(&buf);
+
+    //
+    if (devTextFileLoDebug>0) {
+        printf("%s (devTextFileLo): %s.%06d %s\n", prec->name, datetime, prec->time.nsec/1000, wday);
+    }
+
+    //
+    const int32_t val = prec->val;
+    const int ret = fprintf(fp,
+                            "# devTextFileLo on %s saved\n# %s at %s.%06d %s\n%d\n",
+                            buf.nodename,
+                            prec->name, datetime, prec->time.nsec/1000, wday,
+                            val);
+    if (ret<0) {
+        // write error
+        errlogPrintf("%s (devTextFileLo): No data was written to the file: \"%s\"\n", prec->name, filename);
+        prec->nsev = INVALID_ALARM;
+        prec->nsta = WRITE_ALARM;
+        retval = -1;
+    }
+
+    //
+    prec->udf = FALSE;
+
+    // cleanup
+    fclose(fp);
+    fp = NULL;
+
+    //
+    return retval;
+}
+
+// Register symbol(s) used by IOC core
+epicsExportAddress(int, devTextFileLoDebug);
+
+// end
